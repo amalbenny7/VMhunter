04d6a801be3e48e0288fe2b5b332fcd5b877c4a5
amalbenny7 <innov8devengine.amalbenny@gmail.com>
2024-08-21 13:36:33 +0000

    VM Config

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const connectToDb = require("./config/db");
 const dotenv = require("dotenv");
 const user = require("./routes/user");
 const issueRoutes = require("./routes/issue");
+const path = require("path")
 
 dotenv.config();
 
@@ -13,18 +14,25 @@ connectToDb();
 
 const port = process.env.PORT || 8080;
 
+
+app.use(express.static(path.join(__dirname,"../frontend", "dist")))
+
 app.use(express.json());
 
 app.use(cors());
 
-app.use("/user", user);
+app.get("*", (req,res) => {
+  res.sendFile(path.join(__dirname,"../frontend","dist"))
+})
 
-app.use("/issue", issueRoutes);
+app.use("/API/user", user);
 
-app.get("/", (req, res) => {
+app.use("/API/issue", issueRoutes);
+
+app.get("/API", (req, res) => {
   res.json({ status: 200, message: "API Working" });
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server is up and running at port ${port}`);
 });
--- frontend/src/Utils/makeAPICall.js
+++ frontend/src/Utils/makeAPICall.js
@@ -14,7 +14,7 @@ const makeAPICall = async (options) => {
     "Authorization"
   ] = `Bearer ${localStorage.getItem("accessToken")}`;
   try {
-    axios.defaults.baseURL = "http://localhost:8080";
+    axios.defaults.baseURL = "http://51.21.39.185:8080/API";
 
     const fetchPro = axios({ ...options });
 
